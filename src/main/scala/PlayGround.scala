2b7ec7465e07b8edabbdce6f518d37371699b4fa
RobertootreboR <hrodeberaht@gmail.com>
2018-01-12 19:45:03 +0100

    IDF added but found out I lost information about words

--- src/main/scala/PlayGround.scala
+++ src/main/scala/PlayGround.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.mllib.feature
+import org.apache.spark.mllib.linalg.{SparseVector, Vectors}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
@@ -11,45 +12,74 @@ object PlayGround extends App {
 //    .setMaster("local").setAppName("SVDsearchEnginee")
 //  @transient lazy val sc: SparkContext = new SparkContext(conf)
 //
+////
 //
+//  @transient val spark = SparkSession.builder
+//      .config("spark.sql.warehouse.dir", "file:///C:/Users/admin/IdeaProjects/search/spark-warehouse")
+//    .master("local")
+//    .appName("SVDsearchEnginee")
+//    .getOrCreate()
+//  def toBagOfWords(path: String): RDD[(String, Int)] = {
+//    toBagOfWords(spark.sparkContext.textFile(path))
+//  }
+//
+//  def toBagOfWords(rdd: RDD[String]): RDD[(String, Int)] = {
+//    rdd
+//      .flatMap(_.split("\\W+"))
+//      .map(_.toLowerCase)
+//      .map { word => (word, 1) }
+//      .reduceByKey(_ + _)
+//  }
+//
+////
+////
+////
+//  val x = toBagOfWords(spark.sparkContext.textFile("C:\\Users\\admin\\IdeaProjects\\search\\arts\\*")).collect()
+//  x.foreach(println)
+//
+//  val stemmer = new feature.Stemmer()
+//  val data  = spark.createDataFrame(x).toDF("word","id")
+//  data.printSchema()
+//  val toArray = udf[Array[String], String]( _.split(" "))
+//  val featureDf = data
+//    .withColumn("word", toArray(data("word")))
+//  featureDf.printSchema()
+//  val stemmed =stemmer
+//    .setInputCol("word")
+//    .setOutputCol("stemmed")
+//    .setLanguage("English")
+//    .transform(featureDf)
+//
+//
+//  stemmed.show()
 
-  @transient val spark = SparkSession.builder
-      .config("spark.sql.warehouse.dir", "file:///C:/Users/admin/IdeaProjects/search/spark-warehouse")
-    .master("local")
-    .appName("SVDsearchEnginee")
-    .getOrCreate()
-  def toBagOfWords(path: String): RDD[(String, Int)] = {
-    toBagOfWords(spark.sparkContext.textFile(path))
+  def time[R](block: => R): (R, Long) = {
+    val t0 = System.nanoTime()
+    val result = block // call-by-name
+    val t1 = System.nanoTime()
+    val timediff = t1 - t0
+    println("Elapsed time: " + (timediff) + "ns")
+    (result, timediff)
   }
 
-  def toBagOfWords(rdd: RDD[String]): RDD[(String, Int)] = {
-    rdd
-      .flatMap(_.split("\\W+"))
-      .map(_.toLowerCase)
-      .map { word => (word, 1) }
-      .reduceByKey(_ + _)
+
+  def IDF(v: SparseVector):SparseVector = {
+  val idfFactor = v.size.toDouble / v.numActives.toDouble
+  Vectors.dense(v.toArray.map(_*idfFactor)).toSparse
+}
+
+  def IDF2(v: SparseVector):SparseVector = {
+    val idfFactor = v.size.toDouble / v.numActives.toDouble
+    val newValues = v.values.map(_ * idfFactor)
+    Vectors.sparse(v.size, v.indices, newValues).toSparse
   }
-
-//
-//
-//
-  val x = toBagOfWords(spark.sparkContext.textFile("C:\\Users\\admin\\IdeaProjects\\search\\arts\\*")).collect()
-  x.foreach(println)
-
-  val stemmer = new feature.Stemmer()
-  val data  = spark.createDataFrame(x).toDF("word","id")
-  data.printSchema()
-  val toArray = udf[Array[String], String]( _.split(" "))
-  val featureDf = data
-    .withColumn("word", toArray(data("word")))
-  featureDf.printSchema()
-  val stemmed =stemmer
-    .setInputCol("word")
-    .setOutputCol("stemmed")
-    .setLanguage("English")
-    .transform(featureDf)
-
-
-  stemmed.show()
-
+  time{ (1 to 10).foreach{ i=>
+    val c =Vectors.sparse(200000*i,(1 to 100000*i).toArray,(1 to 100000*i).map(_.toDouble).toArray[Double]).toSparse
+    print(IDF(c).values.length)
+  }}
+  println("duuuuuuuuuuupa")
+  time{ (1 to 10).foreach{ i=>
+    val c =Vectors.sparse(200000*i,(1 to 100000*i).toArray,(1 to 100000*i).map(_.toDouble).toArray[Double]).toSparse
+    print(IDF2(c).values.length)
+  }}
 }
--- src/main/scala/dataframes.scala
+++ src/main/scala/dataframes.scala
@@ -1,7 +1,6 @@
 import org.apache.spark.rdd.RDD
 import SparkConf._
-import breeze.linalg.Matrix
-import org.apache.spark.mllib.linalg
+import com.github.fommil.netlib.BLAS
 import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, IndexedRow, IndexedRowMatrix, RowMatrix}
 import org.apache.spark.mllib.linalg.{SparseVector, Vector, Vectors}
 
@@ -12,40 +11,54 @@ object dataframes extends App{
   val bagMap = x.asMap
   val stemmer = new MyStemmer()
 
-  def transform(array: RDD[(String,Int)]): linalg.Vector = {
-    val indicesval = array.map(record =>(bagMap.get(record._1),record._2))
-      .filter(_._1.isDefined)
-      .map(tup => (tup._1.get,tup._2))
-      .collect()
-    val tup = indicesval.unzip
-    Vectors.sparse(indicesval.length,tup._1.toArray,tup._2.toArray.map(_.toDouble))
-  }
+//  def transform(array: RDD[(String,Int)]): Vector = {
+//    val indicesval = array.map(record =>(bagMap.get(record._1),record._2))
+//      .filter(_._1.isDefined)
+//      .map(tup => (tup._1.get,tup._2))
+//      .collect()
+//    val tup = indicesval.unzip
+//    Vectors.sparse(indicesval.length,tup._1.toArray,tup._2.toArray.map(_.toDouble))
+//  }
 
-  def transform(words: Map[String,Int]): Vector = {
+  def mapToIndex(words: Map[String,Int]): SparseVector = {
     val indicesval = words.map(record =>(bagMap.get(record._1),record._2))
       .filter(_._1.isDefined)
       .map(tup => (tup._1.get,tup._2)).toArray
     val tup = indicesval.unzip
-    Vectors.sparse(indicesval.length,tup._1.toArray,tup._2.toArray.map(_.toDouble))
+    Vectors.sparse(bagMap.size,tup._1.toArray,tup._2.toArray.map(_.toDouble)).toSparse
   }
 
   def countWords(words: Array[String]): Map[String, Int] = {
     words.groupBy(identity).mapValues(_.length)
   }
 
-  def toSparseVector(): RDD[(Vector,Long)] = {
+  def indexRDD(rdd : RDD[String]): RDD[SparseVector] = {
     rdd.map(_.split("\\W+").map(_.toLowerCase))   // transform each file into array of words
       .map(arr =>stemmer.stem(arr.zipWithIndex))  // stem words in files
-     .map(countWords)
-     .map(transform).zipWithIndex()                   // transform arrays to sparse vectorstoSparseVector().map((x,y) => (y,x))
+     .map(countWords)                             // transforms words into Map of word -> occurences
+     .map(mapToIndex)                             // transform arrays to sparse
   }
   println("zrobione parsowanie")
 
-  val c:RDD[IndexedRow] = toSparseVector().map {case (vector,index) => IndexedRow(index,vector) }
+  def IDF(v: SparseVector):SparseVector = {
+    val idfFactor = v.size.toDouble / v.numActives.toDouble
+    val newValues = v.values.map(_ * idfFactor)
+    Vectors.sparse(v.size, v.indices, newValues).toSparse
+  }
+
+  val c:RDD[IndexedRow] = indexRDD(rdd).zipWithIndex().map {case (vector,index) => IndexedRow(index,vector) }.cache()
   println("zrobione indeksowanie")
-  val z = new IndexedRowMatrix(c).toCoordinateMatrix().transpose().toRowMatrix()
-  println(z.rows.count())
-  println("done" + z + "\ndone")
-  print("a")
+  val dupa = indexRDD(rdd).collect()
+  val z = new IndexedRowMatrix(c).toCoordinateMatrix().transpose().toIndexedRowMatrix().rows.collect()
+//  val a = new IndexedRowMatrix(c).rows.first()
+//  val d = new IndexedRowMatrix(c).toCoordinateMatrix().toIndexedRowMatrix().rows.collect()
+x.bagOfWords.take(50).foreach(println)
+  print("\n\n\n")
+z.foreach(a=>  {print(a.index +" : "+ a.vector.toString); print("\n")})
+//  val gh = z.rows.map(_.toSparse).map(IDF)
+//  println("done" + gh.count() + "\ndone")
+//  print("a")
+//  val dx = z.computeSVD(300,computeU = true)
+//  dx.s.toDense.toArray.foreach(println)
 
 }
